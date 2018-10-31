1428e2577268ebb29000d40d45fc6ba93f228604
Simon Melchertsen <warcraft.simon@gmail.com>
2018-10-31 13:19:09 +0100

    Revert "Merge branch 'master' of https://github.com/simon2000234/SDETestGit"
    This reverts commit a7e4ee0ad24749cc0f8dfa702be550c3f1ded3b9, reversing
    changes made to d7f320e2890da370085dd1fd491e372b7941044a.

---
+++ MitTestProjekt/src/mittestprojekt/MitTestProjekt.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mittestprojekt;
+
+/**
+ *
+ * @author Melchertsen
+ */
+public class MitTestProjekt
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        int r = 3;
+        System.out.println("Robot:=" + r + "no shit");
+    }
+    
+}
